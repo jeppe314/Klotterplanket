7bc3b1987dc5d660ab734568b9fb437889f54e5b
jeppe314 <jesperlindqvist93@gmail.com>
2022-12-14 19:40:03 +0100

    loginbutton update

--- components/LoginButton.jsx
+++ components/LoginButton.jsx
@@ -1,5 +1,13 @@
 import React from "react"
+import Link from "next/link"
 
 export default function LoginButton() {
-    return <a href="/api/auth/login">Login</a>
+    return <Link href="/api/auth/login">Login</Link>
 }
+
+
+    //    ;<Link href="/api/auth/logout">
+    //        <AiOutlineLogout size="50px" color="darkred" />
+    //    </Link>
+
+    //    <a href="/api/auth/login">Login</a>
